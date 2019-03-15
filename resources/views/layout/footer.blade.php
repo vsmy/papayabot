e88ee0f028b811fd07d98320669d96c4beec3ae0
vsmy <40499344+vsmy@users.noreply.github.com>
2019-03-15 19:08:56 +0200

    add header section

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,38 @@
+<footer>
+  <div class="container justify-content-center">
+      <div class="row justify-content-center">
+            <div class="col-md-3">
+                <h4 class="title text-center" style="color: #2b137f; font-weight: 900">Papayabot</h4>
+            </div>
+          <div class="col-md-6">
+              <div class="d-flex justify-content-around">
+                  <a style="    font-weight: 900;" href="#">About</a>
+                  <a style="    font-weight: 900;" href="#">Tearms and conditions </a>
+                  <a style="    font-weight: 900;" href="#">FAQ</a>
+              </div>
+
+              <p class="subtitle text-center">Do you have questions? Mail us at: papayabot.com</p>
+          </div>
+          <div class="col-md-3" >
+              <div class="social-icon  ">
+                  <ul class="navbar-nav ml-auto">
+                      <li>
+                          <a class="icon youtube waves-effect waves-light" target="_blank" href="https://www.youtube.com/channel/UCTfsH5M5BmgpLW1WrMrNm2A"></a>
+                      </li>
+                      <li>
+                          <a class="icon facebook waves-effect waves-light" target="_blank" href="https://www.facebook.com/intobiapp/"></a>
+                      </li>
+                      <li>
+                          <a class="icon instagram waves-effect waves-light" target="_blank" href="https://www.instagram.com/intobiapp/"></a>
+                      </li>
+                  </ul>
+              </div>
+          </div>
+      </div>
+<div class="row justify-content-center">
+    <div class="subtitle">
+        3242343243243243
+    </div>
+</div>
+  </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,92 +8,126 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+        <header>
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 d-flex flex-column justify-content-sm-between">
+                        <div class="logo">
+                            <h5 class="title-logo">Papayabot</h5>
+                        </div>
+                        <div class="header-title">
+                            <h1 class="title black">
+                                The multicurrency wallet for Telegram messenger
+                            </h1>
+                            <div class="subtitle">
+                                Make payments for free inside <b>Telegram</b> using over <b>20 cryptos</b> such as Bitcoin or Stellar Lumens
+                            </div>
+                            <button class="btn btn-papayabot ">Try it now</button>
+                        </div>
+                        <div class="text-xs-center">
+                        </div>
+                    </div>
+                    <div class="col-md-6">
+                        <div class="home-bg"></div>
+                    </div>
                 </div>
             </div>
-        </div>
+        </header>
+
+
+        <section>
+            <div class="container">
+                <div class="row align-items-center" >
+                    <div class="col-md-6">
+                        <h1 class="title black">
+                       title
+                        </h1>
+                        <h5 class="subtitle">
+                            subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                        </h5>
+                    </div>
+                </div>
+                <div class="row align-items-center" >
+                 <div class="col-md-4">
+                     <div class="test_icon"></div>
+                     <h3 class="title black">
+                         Title
+                     </h3>
+                     <h5 class="subtitle">
+                         subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                     </h5>
+                 </div>
+                 <div class="col-md-4">
+                     <div class="test_icon"></div>
+                     <h3 class="title black">
+                         Title
+                     </h3>
+                     <h5 class="subtitle">
+                         subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                     </h5>
+                 </div>
+                 <div class="col-md-4">
+                     <div class="test_icon"></div>
+                     <h3 class="title black">
+                         Title
+                     </h3>
+                     <h5 class="subtitle">
+                         subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                     </h5>
+                 </div>
+                </div>
+            </div>
+        </section>
+
+
+        <section>
+            <div class="container mr-0">
+                <div class="row align-items-center" >
+                    <div class="col-md-6">
+                        <h1 class="title black">
+                            Title
+                        </h1>
+                        <h5 class="subtitle">
+                            subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                        </h5>
+                    </div>
+                </div>
+                <div class="row align-items-center" >
+                    <div class="col-md-4">
+                        <div class="test_icon"></div>
+                        <h3 class="title black">
+                            Title
+                        </h3>
+                        <h5 class="subtitle">
+                            subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                        </h5>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="test_icon"></div>
+                        <h3 class="title black">
+                            Title
+                        </h3>
+                        <h5 class="subtitle">
+                            subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                        </h5>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="test_icon"></div>
+                        <h3 class="title black">
+                            Title
+                        </h3>
+                        <h5 class="subtitle">
+                            subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle subtitle  subtitle
+                        </h5>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <br><br><br>
+        @include('layout.footer')
     </body>
 </html>
